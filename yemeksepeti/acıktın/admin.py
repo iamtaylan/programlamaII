98570d674cf964881cd58adb0a493ce018954517
iamtaylan <ozturktaylan266@gmail.com>
2022-12-24 13:51:10 +0300

    admin congig

--- yemeksepeti/acıktın/admin.py
+++ yemeksepeti/acıktın/admin.py
@@ -3,8 +3,19 @@ from acıktın.models import *
 
 # Register your models here.
 
-admin.site.register(Restoran)
-admin.site.register(Person)
+class RestoranAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    prepopulated_fields = {'slug': ('title',) }
+    list_filter = ('genres','language',)
+    search_fields = ('title','description',)
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('full_name','gender','duty_type')
+    list_filter = ('gender','duty_type')
+    search_fields = ('first_name','last_name')
+
+admin.site.register(Restoran, RestoranAdmin)
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Contact)
 admin.site.register(Genre)
 admin.site.register(Video)
--- yemeksepeti/acıktın/models.py
+++ yemeksepeti/acıktın/models.py
@@ -16,6 +16,7 @@ class Contact(models.Model):
     def __str__(self):
         return self.address
 
+
 class Person(models.Model):
 
     genders = (
@@ -35,24 +36,38 @@ class Person(models.Model):
     biography = CharField(max_length=3000)
     image_name = models.CharField(max_length=50)
     date_of_birth = models.DateField()
-    gender = models.CharField(max_length=1, choices=genders)
-    duty_type = models.CharField(max_length=1, choices=duty_types)
+    gender = models.CharField("cinsiyet",max_length=1, choices=genders)
+    duty_type = models.CharField("görev",max_length=1, choices=duty_types)
     contact = models.OneToOneField(Contact, on_delete=models.CASCADE, null=True, blank=True)
 
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    full_name.fget.short_description = "ad soyad"
+
+    class Meta:
+        verbose_name = "Kişi"
+        verbose_name_plural = "Kişiler"
+
     def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.duty_types[int(self.duty_type)-1][1]})"
+        return f"{self.first_name} {self.last_name} ({self.duty_types[int(self.duty_type)-1][1]})" 
 
 class Restoran(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(validators = [MinLengthValidator(20)])
-    image_name = models.CharField(max_length=50)
+    title = models.CharField("Başlık",max_length=100)
+    description = models.TextField("Özet",validators = [MinLengthValidator(20)])
+    image_name = models.CharField("Resim",max_length=50)
     image_cover = models.CharField(max_length=50)
     date = models.DateField()
     slug = models.SlugField(unique=True,db_index=True)
     budget = models.DecimalField(max_digits=19,decimal_places=2)
-    acik_kapali = models.CharField(max_length=100)
+    language = models.CharField(max_length=100)
     people = models.ManyToManyField(Person)
     genres = models.ManyToManyField(Genre)
+
+    class Meta:
+        verbose_name = "Restoran"
+        verbose_name_plural = "Restoranlar"
 
     def __str__(self):
         return self.title
