77d9677964e0c88eb7b318246c942819d0c42b71
iamtaylan <ozturktaylan266@gmail.com>
2022-12-27 16:06:35 +0300

    .

--- yemeksepeti/acıktın/urls.py
+++ yemeksepeti/acıktın/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home_page"),
-    path("account/", include("account.urls")),
+    path("account/", include("account.urls"), name="profil"),
     path("restoranlar", views.restoranlar, name="restoran_page"),
     path("restoranlar/<slug:slug>", views.menuler, name="menuler_name")
 
